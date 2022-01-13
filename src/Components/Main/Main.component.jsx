4051ba1c8fa58c948dcdc1512519b78bb1d949d4
yahbouss <47297430+yahbouss@users.noreply.github.com>
2022-01-13 14:25:04 +0100

    Implemented the automatic bonus

--- src/Components/Main/Main.component.jsx
+++ src/Components/Main/Main.component.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import cookie from "../../assets/cookie-svgrepo-com.svg"
 import "./Main.scss"
 
 const Main = () => {
     const [click, setclick] = useState(0)
+    const [step, setstep] = useState(1)
+
+    useEffect(() => {
+        setInterval(()=>{
+            setstep(step*2)
+            
+        }, 20) 
+
+        setTimeout(()=>{
+            setstep(0)
+        },5)
+
+    }, [step])
+
+    const handleClick = ()=>{
+        setclick(click+step)
+    }
 
     return (
         <div className='container'>
             <p className='clicks'>{click}</p>
-            <img onClick={e=>setclick(click+1)} className='cookie' src={cookie} alt="cookie"/>
+            <p className='clicks'>step: {step}</p>
+            <img onClick={handleClick} className='cookie' src={cookie} alt="cookie"/>
         </div>
     )
 }
